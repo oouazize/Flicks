928bbf9b86f9a72cc7bf500969cea5797ab209b4
Oussama Ouazize <oouazize@student.1337.ma>
2023-02-16 18:25:54 +0100

    add mode and alive movie-list

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 const arrows = document.querySelectorAll('.arrow-container');
 const movielist = document.querySelectorAll('.movie-list');
 
+var width = Math.round(innerWidth*7/2880);
+
 var j = 0;
+const itemNumber = 14;
+var variable = -102;
+
 arrows.forEach((arrow, i) => {
     arrow.addEventListener('click', () => {
-        arrow.classList.add("hideMeAfterClick");
+        width = Math.round(innerWidth * 7 / 2880);
         movielist[i].style.transition = "all .8s";
-        if (j === 1) {
-            movielist[i].style.transform = "translateX(0%)";
-            arrow.style.transform = "translateX(0em)";
-            j = 0;
+        if (itemNumber - (width + j * width) <= 0) {
+            movielist[i].style.transform = "translateX(0)";
+            j = 0; variable = -102;
         }
         else {
-            movielist[i].style.transform = "translateX(-100%)";
-            arrow.style.transform = "translateX(174.6em)";
-            j++;
+            movielist[i].style.transform = `translateX(${variable}%)`;
+            j++; variable -= 102;
         }
     });
+});
+
+const toggle = document.querySelector('.toggle');
+const items = document.querySelectorAll(".navbar, .sidebar, .featured-content, .toggle, .boDy");
+
+toggle.addEventListener('click', () => {
+    toggle.children[2].classList.toggle('ball-toggle');
+    items.forEach(item => item.classList.toggle('active'));
 });
