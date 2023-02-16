e2a7ee51c95f3ab0c765b932e061e5107bd6d399
Oussama Ouazize <oouazize@student.1337.ma>
2023-02-16 20:59:28 +0100

    add arrow left

--- script.js
+++ script.js
@@ -1,24 +1,44 @@
-const arrows = document.querySelectorAll('.arrow-container');
+const arrowsRight = document.querySelectorAll('.arrow-right-container');
+const arrowsLeft = document.querySelectorAll('.arrow-left-container');
 const movielist = document.querySelectorAll('.movie-list');
 
-var width = Math.round(innerWidth*7/2880);
+var width = Math.round(innerWidth * 7 / 2880);
 
 var j = 0;
 const itemNumber = 14;
 var variable = -102;
+var generalVar = 0;
 
-arrows.forEach((arrow, i) => {
+arrowsRight.forEach((arrow, i) => {
     arrow.addEventListener('click', () => {
         width = Math.round(innerWidth * 7 / 2880);
         movielist[i].style.transition = "all .8s";
+        if (generalVar === 2) variable -= 102;
+        generalVar = 0;
         if (itemNumber - (width + j * width) <= 0) {
             movielist[i].style.transform = "translateX(0)";
             j = 0; variable = -102;
         }
         else {
             movielist[i].style.transform = `translateX(${variable}%)`;
-            j++; variable -= 102;
+            variable -= 102; j++;
         }
+        setTimeout(() => {
+            (variable === -102) ? arrowsLeft[i].classList.remove('hidden-arrow') : arrowsLeft[i].classList.add('hidden-arrow');}
+            , 800);
+    });
+});
+
+arrowsLeft.forEach((arrow, i) => {
+    arrow.addEventListener('click', () => {
+        (!generalVar) ? variable += 204 : variable += 102;
+        width = Math.round(innerWidth * 7 / 2880);
+        movielist[i].style.transition = "all .8s";
+        movielist[i].style.transform = `translateX(${variable}%)`;
+        generalVar = 2; j--;
+        setTimeout(() => {
+            (variable === 0) ? arrowsLeft[i].classList.remove('hidden-arrow') : arrowsLeft[i].classList.add('hidden-arrow');}
+            , 800);
     });
 });
 
